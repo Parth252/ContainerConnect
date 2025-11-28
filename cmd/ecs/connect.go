6ce1be093a1f4007f1512b57c192f1b4047254ff
Parth252 <parthlokhande24@gmail.com>
2025-11-28 22:37:01 +0530

    adding sample connect code

--- cmd/ecs/connect.go
+++ cmd/ecs/connect.go
@@ -1,1 +1,19 @@
 package ecs
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+var ConnectCmd = &cobra.Command{
+	Use:   "connect",
+	Short: "Connect to a running container in ECS",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Connecting to ECS container...")
+	},
+}
+
+func init() {
+	EcsCmd.AddCommand(ConnectCmd)
+}
