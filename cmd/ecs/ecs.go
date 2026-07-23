2fbd107e90f403f2ed5c321acec23f6d370ec821
Parth252 <parthlokhande24@gmail.com>
2026-07-23 20:03:17 +0530

    readding ecs file

---
+++ cmd/ecs/ecs.go
@@ -0,0 +1,16 @@
+package ecs
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// ecsCmd represents the "ecs" command under root
+var EcsCmd = &cobra.Command{
+	Use:   "ecs",
+	Short: "Commands related to AWS ECS clusters and services",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("ECS command selected — use a subcommand like 'list' or 'exec'")
+	},
+}
